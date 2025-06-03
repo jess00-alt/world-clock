7776bc880e7c70c4f03559d2e2a0717e96b60940
Jessica Wessels <jessica.wessels@icloud.com>
2025-06-03 02:18:12 +0200

    Added javascript to project

---
+++ script.js
@@ -0,0 +1,29 @@
+function updateTime() {
+
+let sãoPauloElement = document.querySelector("#são-paulo");
+let sãoPauloDateElement = document.querySelector("#são-paulo-date");
+let sãoPauloTimeElement = document.querySelector("#brazil-timezone");
+let sãoPauloTime= moment().tz("America/Sao_Paulo"
+)
+sãoPauloDateElement.innerHTML = sãoPauloTime.format("MMMM Do YYYY");
+sãoPauloTimeElement.innerHTML = sãoPauloTime.format("h:mm:ss [<small>]A[</small]");
+
+let tokyoElement = document.querySelector("#tokyo");
+let tokyoDateElement = document.querySelector("#tokyo-date");
+let tokyoTimeElement = document.querySelector("#japan-timezone");
+let tokyoTime= moment().tz("Asia/Tokyo"
+)
+tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small]");
+
+let capeTownElement = document.querySelector("#cape-town");
+let capeTownDateElement = document.querySelector("#cape-town-date");
+let capeTownTimeElement = document.querySelector("#south-africa-timezone");
+let capeTownTime= moment().tz("Africa/Johannesburg"
+)
+capeTownDateElement.innerHTML = capeTownTime.format("MMMM Do YYYY");
+capeTownTimeElement.innerHTML = capeTownTime.format("h:mm:ss [<small>]A[</small]");
+}
+
+updateTime();
+setInterval(updateTime, 1000)
